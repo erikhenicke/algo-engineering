012117cffa0a8dc2255c8c8b3bd6f35f81427ff8
ru84rin <erik.henicke@uni-jena.de>
2024-10-15 11:39:21 +0200

    coding warm-up: pi approximation

---
+++ answers/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by erik on 15.10.24.
+//
+
+#include <iostream>
+#include <ostream>
+#include "pi_approximation.h"
+
+using namespace std;
+
+int main() {
+    pi_numerical_integration(100000000);
+    pi_numerical_integration_parallel(100000000);
+    pi_monte_carlo(10000000);
+}
---
+++ answers/pi_approximation.h
@@ -0,0 +1,12 @@
+//
+// Created by erik on 15.10.24.
+//
+
+#ifndef PI_NUMERICAL_INTEGRATION_H
+#define PI_NUMERICAL_INTEGRATION_H
+
+void pi_numerical_integration(int);
+void pi_numerical_integration_parallel(int);
+void pi_monte_carlo(int);
+
+#endif
