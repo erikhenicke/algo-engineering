610a6da9c189041c24cc555fe2825e1a76385773
ru84rin <erik.henicke@uni-jena.de>
2024-11-08 22:01:23 +0100

    assignment: first benchmarking (week 4)

--- answers/week04/week04.cpp
+++ answers/week04/week04.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "coding-warmup/coding_warmup.h"
 #include "exam-exercises/exam_exercises.h"
+#include "matplotlibcpp.h"
 
 using namespace std;
+namespace plt = matplotlibcpp;
 
 int main() {
-  // cout << string(80, '#') << endl;
-  // cout << "Coding warmup:" << endl;
-  // cout << string(80, '#') << endl;
-  // run_merge_sort();
-  // run_merge();
+  cout << string(80, '#') << endl;
+  cout << "Coding warmup:" << endl;
+  cout << string(80, '#') << endl;
+  run_merge_sort();
+  run_merge();
 
   cout << '\n' << string(80, '#') << endl;
   cout << "Benchmarking:" << endl;
